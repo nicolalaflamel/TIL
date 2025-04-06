b5b77dfb80633bc2e9c08db9c5790a48fd02418a
Yuki Maruyama <yukmaruyamu@gmail.com>
2025-04-06 22:44:25 +0900

    Add: バックエンド環境構築
    バックエンド環境構築
    
    環境構築
    バックエンドDB接続

---
+++ AutoDevLog/AutoDevLog-back/auto-dev-log/src/main/java/com/conv/daily/AutoDevLogApplication.java
@@ -0,0 +1,14 @@
+package com.conv.daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDevLogApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		SpringApplication.run(AutoDevLogApplication.class, args);
+	}
+
+}
---
+++ AutoDevLog/AutoDevLog-back/auto-dev-log/src/test/java/com/conv/daily/AutoDevLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conv.daily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoDevLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
